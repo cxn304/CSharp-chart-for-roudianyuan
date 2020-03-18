fa0d96e3df13da3e7f8abf036c9984d1763469ba
cxn <799538436@qq.com>
2020-03-18 16:10:25 +0800

    初始

---
+++ chart1/Form1.Designer.cs
@@ -0,0 +1,109 @@
+﻿namespace chart1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.btnstart = new System.Windows.Forms.Button();
+            this.btnstop = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // chart1
+            // 
+            chartArea1.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea1);
+            legend1.Name = "Legend1";
+            this.chart1.Legends.Add(legend1);
+            this.chart1.Location = new System.Drawing.Point(58, 22);
+            this.chart1.Name = "chart1";
+            series1.ChartArea = "ChartArea1";
+            series1.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Point;
+            series1.Legend = "Legend1";
+            series1.Name = "random";
+            series1.XValueType = System.Windows.Forms.DataVisualization.Charting.ChartValueType.Time;
+            this.chart1.Series.Add(series1);
+            this.chart1.Size = new System.Drawing.Size(800, 400);
+            this.chart1.TabIndex = 0;
+            this.chart1.Text = "chart1";
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // btnstart
+            // 
+            this.btnstart.Location = new System.Drawing.Point(883, 52);
+            this.btnstart.Name = "btnstart";
+            this.btnstart.Size = new System.Drawing.Size(117, 32);
+            this.btnstart.TabIndex = 1;
+            this.btnstart.Text = "开始";
+            this.btnstart.UseVisualStyleBackColor = true;
+            this.btnstart.Click += new System.EventHandler(this.btnstart_Click);
+            // 
+            // btnstop
+            // 
+            this.btnstop.Location = new System.Drawing.Point(883, 136);
+            this.btnstop.Name = "btnstop";
+            this.btnstop.Size = new System.Drawing.Size(117, 35);
+            this.btnstop.TabIndex = 2;
+            this.btnstop.Text = "结束";
+            this.btnstop.UseVisualStyleBackColor = true;
+            this.btnstop.Click += new System.EventHandler(this.btnstop_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1043, 459);
+            this.Controls.Add(this.btnstop);
+            this.Controls.Add(this.btnstart);
+            this.Controls.Add(this.chart1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Button btnstart;
+        private System.Windows.Forms.Button btnstop;
+    }
+}
+
---
+++ chart1/Form1.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Windows.Forms.DataVisualization.Charting;
+
+namespace chart1
+{
+
+    public partial class Form1 : Form
+    {
+
+        private Queue<double> dataQueue = new Queue<double>(100);   // 显示100个点
+        private Queue<string> timeQueue = new Queue<string>(100);
+
+        private int num = 1;//每次删除增加几个点
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            chart1.Series[0].ChartType = SeriesChartType.Point;
+            chart1.Series[0].IsValueShownAsLabel = true;    // 设置是否在Chart中显示坐标点值
+            this.chart1.ChartAreas[0].AxisY.Minimum = 0;
+            this.chart1.ChartAreas[0].AxisY.Maximum = 100;
+            this.chart1.ChartAreas[0].AxisX.Interval = 5;
+            chart1.ChartAreas[0].Name = "绘图";
+            chart1.ChartAreas[0].AxisX.Title = "当前时间(H:M:S)";   // x轴名称
+            chart1.BorderlineWidth = 2;
+            chart1.Series[0].Name = "数据1"; //设置数据名称(text)
+            //chart1.Width = 800;
+            //chart1.Height = 400;
+        }
+
+        private void btnstart_Click(object sender, EventArgs e)
+        {
+            chart1.Series[0].XValueType = ChartValueType.Time;  // X坐标轴数据格式
+            timer1.Enabled = true;
+            this.timer1.Start();
+            chart1.Series[0].Points.Clear();
+        }
+
+        private void btnstop_Click(object sender, EventArgs e)
+        {
+            timer1.Stop();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            // DateTime.Now.ToLongTimeString().ToString();  // 格式20:13:52
+            UpdateQueueValue();
+            this.chart1.Series[0].Points.Clear();   // 每次更新之前画的图
+            for (int i = 0; i < dataQueue.Count; i++)
+            {
+                this.chart1.Series[0].Points.AddXY(timeQueue.ElementAt(i), dataQueue.ElementAt(i));
+            }
+        }
+
+
+        private void UpdateQueueValue()
+        {
+
+            if (dataQueue.Count > 100)   // 表示队列有100个点
+            {
+                //先出列
+                for (int i = 0; i < num; i++)
+                {
+                    dataQueue.Dequeue();
+                    timeQueue.Dequeue();
+                }
+            }
+
+            Random r = new Random();
+            for (int i = 0; i < num; i++)
+            {
+                dataQueue.Enqueue(r.Next(0, 100));
+                timeQueue.Enqueue(DateTime.Now.ToLongTimeString().ToString());
+            }
+
+            string bmpName = "0" + ".jpeg";
+            // Save image
+            chart1.SaveImage(bmpName, ChartImageFormat.Jpeg);
+        }
+
+
+    }
+}
